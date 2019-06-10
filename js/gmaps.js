cd80ca29abe56daaf035c9a47ffa314bead8d989
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-06-10 14:44:36 +0200

    Finish task 3 - connecting google maps with flickity + blocking centering map upon marker click

--- js/gmaps.js
+++ js/gmaps.js
@@ -1,18 +1,32 @@
 window.initMap = function() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: slideList[0].coords
   });
   const markers = [];
   const infoDiv = document.querySelector('#info');
+
+  //   Zmienna abort posluzy do blokowania centrowania po kliknieciu w marker
+  let abort = false;
+
   slideList.forEach((x, id) => {
     markers[id] = new google.maps.Marker({ position: x.coords, map: map });
     markers[id].addListener('click', function() {
+      abort = true;
       infoDiv.style.visibility = 'visible';
       infoDiv.innerHTML = `You clicked on marker ${id + 1}`;
+      flkty.selectCell(id);
       setTimeout(function() {
         infoDiv.style.visibility = 'hidden';
       }, 2000);
     });
+  });
+
+  flkty.on('change', function(index) {
+    if (!abort) {
+      map.panTo(slideList[index].coords);
+      map.setZoom(4);
+    }
+    abort = false;
   });
 };
